1cd0a93fda70795110187282b4a74a3109c16e05
adekto <iwert.haex@gmail.com>
2016-11-20 01:16:18 +0100

    changed deprecated  getn for #

--- example/scenery.lua
+++ example/scenery.lua
@@ -1,7 +1,7 @@
 scenery = {stack={}}
 function scenery.load(sceneFile, sceneName)
-	if scenery.stack[table.getn(scenery.stack)] ~= sceneFile then
-  		scenery.stack[table.getn(scenery.stack)+1] = sceneFile
+	if scenery.stack[#scenery.stack] ~= sceneFile then
+  		scenery.stack[#scenery.stack+1] = sceneFile
   	end
   	local sceneryChunk = love.filesystem.load(sceneFile..".lua")
   	sceneryChunk()
@@ -9,6 +9,6 @@ function scenery.load(sceneFile, sceneName)
   	scenery.scene.load()
 end
 function scenery.exit()
-	table.remove(scenery.stack,table.getn(scenery.stack))
-	scenery.load(scenery.stack[table.getn(scenery.stack)])
+	table.remove(scenery.stack,#scenery.stack)
+	scenery.load(scenery.stack[#scenery.stack])
 end
